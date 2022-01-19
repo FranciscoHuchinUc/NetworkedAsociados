ccd7311cf5a89da3351892875a742fd39f935ee6
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-01-18 20:53:31 -0600

    mediaQuery

--- js/main.js
+++ js/main.js
@@ -22,3 +22,64 @@ function linkAction(){
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+// 
+
+const scholarshipsContent = document.getElementsByClassName('scholarships__content'),
+scholarshipsHeader = document.querySelectorAll('.scholarships__header')
+
+function toggleScholarships() {
+    let itemClass = this.parentNode.className
+
+    for(i = 0; i < scholarshipsContent.length; i++){
+        scholarshipsContent[i].className = 'scholarships__content scholarships__close'
+    }
+    if(itemClass == 'scholarships__content scholarships__close') {
+        this.parentNode.className = 'scholarships__content scholarships__open'
+    }
+}
+
+scholarshipsHeader.forEach((el) => {
+    el.addEventListener('click', toggleScholarships)
+})
+
+// 
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+// 
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+// 
+
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
